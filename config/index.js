9ff7990ae29946c351832c0e61b492673a067325
爱怡 <113652396+aiyi0813@users.noreply.github.com>
2022-09-19 16:29:53 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oCb3H5h3LbR518fJFwYNDF7EePfY",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "	v3jbZsq-OG7OWSoHZmWdUiM2Gfhhzvta0e2XmQEEdso",
+      useTemplateId: "v3jbZsq-OG7OWSoHZmWdUiM2Gfhhzvta0e2XmQEEdso",
       // 所在省份
       province: "陕西",
       // 所在城市
